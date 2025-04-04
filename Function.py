da4f4d19ff7ddff3b0be8a59fdb0d5453db6e273
Joaquin Torino <jtorinor6@gmail.com>
2025-04-04 15:21:36 -0300

    Function class

--- Function.py
+++ Function.py
@@ -1,6 +1,21 @@
 class Function(object):
     def __init__(self, name):
         self.name = name
+        self.Xdata = []
+        self.Ydata = []
+        self.periodic: bool = False
+        self.period: float = 0.0
 
-    periodic: bool = False
+    def IsPeriodic(self) -> bool:
+        return self.periodic
+
+    def SetPeriodic(self, periodic: bool):
+        self.periodic = periodic
+
+    def GetXData(self) -> list:
+        return self.Xdata
+
+    def GetYData(self) -> list:
+        return self.Ydata
+
 
