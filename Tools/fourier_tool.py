ef3d14a911e66797ffe78a37a0bb09f17b782f05
Facundo Caviglia <fcaviglia@itba.edu.ar>
2025-04-11 16:51:37 -0300

    cambios del fourier plotter

--- Tools/fourier_tool.py
+++ Tools/fourier_tool.py
@@ -1,97 +1,59 @@
-from Tool import *
-from numpy.fft import fft, fftfreq
 import numpy as np
+import dearpygui.dearpygui as img
+from Tool import *
 
 class FourierTool(Tool):
-    def __init__(self, editor, uuid, signal=None):
+    def __init__(self, editor, uuid):
         Tool.__init__(self, name="FourierTool", editor=editor, uuid=uuid)
-        self.signal = signal
-        self.sampled_signal = None
-        self.FFTs_freqs = []
-        self.FFTs_magnitudes = []
-        self.fft_mag_label = ""
-
         self.Init(self.Run)
+        self.signal = None
 
     def Run(self):
-        def update_plot_components(plot_tag, y_axis_tag, x_axis_tag, series_tag):
-            signal = self.editor.selected_signal
-            if signal is None:
-                raise AssertionError("No signal is copied")
-            
-            # Update plot title
-            img.set_item_label(plot_tag, signal.name)
-            
-            # Update axis label
-            img.set_item_label(x_axis_tag, "rad/s")
-            img.set_item_label(y_axis_tag, "V.s")
-            
-            self.calculate_FFT()    # Actualizo valores de la FFT (Magnitudes y dominio de frecuencia)
-            
-            # Update line series data
-            xdata, ydata = self.FFTs_freqs, self.FFTs_magnitudes
-            img.set_value(series_tag, [xdata, ydata])
-        
-        if self.signal != None:
-            self.calculate_FFT()
 
-        with img.plot(label=str(self.name + str(self.toolId)), width=400, height=240) as plot_id:
+        with img.plot(label="Señal en el Tiempo", width=-1, height=200, parent=self.tab):
             img.add_plot_legend()
-            x_axis = img.add_plot_axis(img.mvXAxis, label="rad/s")
-            y_axis = img.add_plot_axis(img.mvYAxis, label="V.s")
-            series = img.add_line_series([] if self.signal == None else self.FFTs_freqs,
-                                         [] if self.signal == None else self.FFTs_magnitudes,
-                                         parent=y_axis)
+            xt = img.add_plot_axis(img.mvXAxis)
+            yt = img.add_plot_axis(img.mvYAxis)
+            self.serie_time = img.add_line_series([], [], parent=yt)
 
-        img.add_button(label="Update Plot", tag="UpdatePlot"+self.name+str(self.toolId), callback=lambda: update_plot_components(
-            plot_tag=plot_id,
-            y_axis_tag=y_axis,
-            x_axis_tag=x_axis,
-            series_tag=series,
-        ))
-        
-    def calculate_FFT(self):
-        signal = self.editor.selected_signal
-        if signal is None:
-            raise AssertionError("No signal is copied")
-        
-        # ------------------ DFT ------------------
-        
-        X_dft = self.DFT()
-        
-        # ------------------ FFT ------------------
-        x_data, y_data = signal.GetData()
-        dx = np.diff(x_data)
-        
-        # Dominio temporal
-        dt = dx[0]
-        N = len(x_data)
-        t = x_data
-        f_t = y_data
-        
-        X_fft = fft(y_data)
+        with img.plot(label="FFT centrada", width=-1, height=200, parent=self.tab):
+            img.add_plot_legend()
+            self.xf = img.add_plot_axis(img.mvXAxis, label="Frecuencia [Hz]")
+            self.yf = img.add_plot_axis(img.mvYAxis, label="Amplitud")
+            self.serie_fft = img.add_line_series([], [], parent=self.yf)
 
-        # Frecuencia en Hz (omega = 2πf)
-        freqs = fftfreq(N, d=dt)
-        omega_dft = 2 * np.pi * freqs
+            img.add_button(label="Pegar Señal", callback=self.paste_signal, parent=self.tab)
 
-        # Sólo mitad positiva para DFT/FFT
-        half = N // 2
-        omega_dft_pos = omega_dft[:half]
-        X_dft_mag = np.abs(X_dft[:half]) * dt
-        X_fft_mag = np.abs(X_fft[:half]) * dt / N   # Normalización
-        
-        self.FFTs_freqs = omega_dft_pos
-        self.FFTs_magnitudes = X_fft_mag
-        #self.fft_mag_label = signal.x_label + " . " + signal.y_label
-        
-        
-    def DFT(self):
-        signal = self.editor.selected_signal
-        x_data, y_data = signal.GetData()
-        N = len(x_data)
-        X = np.zeros(N, dtype=complex)
-        for k in range(N):
-            for n in range(N):
-                X[k] += y_data[n] * np.exp(-2j * np.pi * k * n / N)
-        return X
+
+    def paste_signal(self):
+        self.signal = self.editor.selected_signal
+        if self.signal is None:
+            raise AssertionError("No signal copied")
+
+        x, y = self.signal.GetData()
+        img.set_value(self.serie_time, [list(x), list(y)])
+        self.apply_fft()
+
+    def apply_fft(self):
+        if self.signal is None:
+            return
+
+        x, y = self.signal.GetData()
+        fs = 1 / (x[1] - x[0])
+        Nfft = 2**18
+
+        Xf = np.fft.fft(y, Nfft)
+        freq = np.fft.fftfreq(Nfft, d=1/fs)
+        Xf = np.fft.fftshift(Xf)
+        freq = np.fft.fftshift(freq)
+
+        mag = np.abs(Xf)
+
+        # Detectar la frecuencia de mayor magnitud
+        idx_max = np.argmax(mag)
+        Fmax = np.abs(freq[idx_max])
+
+        # Limitar entre -10*Fmax y 10*Fmax
+        img.set_axis_limits(self.xf, -10 * Fmax, 10 * Fmax)
+
+        img.set_value(self.serie_fft, [list(freq), list(mag)])
